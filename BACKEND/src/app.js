0454f6ba444d7c5f8ad33dbe241e273139dc8a43
Sanket Vishwakarma <your-email@example.com>
2026-03-26 10:10:23 +0530

    Just Updated baseURL

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -8,7 +8,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://moodify-frontend-pearl.vercel.app/",
     credentials: true,
   }),
 );
