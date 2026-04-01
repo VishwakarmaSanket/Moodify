81fde5989402c911313ff4ccb530bbb7bd8eb68d
Sanket Vishwakarma <your-email@example.com>
2026-04-01 17:13:26 +0530

    Add referrerPolicy attribute to image in LeftLogin component for enhanced security

--- FRONTEND/src/features/auth/components/LeftLogin.jsx
+++ FRONTEND/src/features/auth/components/LeftLogin.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const LeftLogin = () => {
   return (
     <div className="left">
-      <img src="https://ik.imagekit.io/64elqj7rx1/loginPage.png" alt="Login" />
+      <img
+        src="https://ik.imagekit.io/64elqj7rx1/loginPage.png"
+        alt="Login"
+        referrerPolicy="no-referrer"
+      />
       <div className="overlay-content">
         <h2>Feel the Music</h2>
         <p>Let your mood choose your vibe 🎧</p>
